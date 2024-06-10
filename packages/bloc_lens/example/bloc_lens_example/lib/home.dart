84c9285b307c3ac0455aa7b45066f81193fa17df
PiotrRogulski <piotr.rogulski@leancode.pl>
2024-06-10 12:25:59 +0200

    Refactor example home page

--- packages/bloc_lens/example/bloc_lens_example/lib/home.dart
+++ packages/bloc_lens/example/bloc_lens_example/lib/home.dart
@@ -22,55 +22,64 @@ class Home extends StatelessWidget {
         itemCount: counters.get().length,
         padding: const EdgeInsets.all(8),
         separatorBuilder: (context, index) => const SizedBox(height: 8),
-        itemBuilder: (context, index) {
-          final counter = counters.at(index);
-          if (counter == null) {
-            return const SizedBox.shrink();
-          }
+        itemBuilder: (context, index) => _CounterCard(index: index),
+      ),
+    );
+  }
+}
 
-          return Card(
-            clipBehavior: Clip.antiAlias,
-            child: ListTile(
-              contentPadding: const EdgeInsetsDirectional.symmetric(
-                horizontal: 16,
-              ),
-              title: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton.outlined(
-                    icon: const Icon(Icons.remove),
-                    onPressed: () => counter.set(counter.get() - 1),
-                  ),
-                  const SizedBox(width: 8),
-                  IconButton.outlined(
-                    icon: const Icon(Icons.add),
-                    onPressed: () => counter.set(counter.get() + 1),
-                  ),
-                  const SizedBox(width: 16),
-                  Text(counter.get().toString()),
-                ],
-              ),
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton.outlined(
-                    onPressed: () {
-                      counters.removeAt(index);
-                    },
-                    isSelected: false,
-                    icon: const Icon(Icons.delete),
-                  ),
-                  const SizedBox(width: 16),
-                  const Icon(Icons.arrow_forward_ios),
-                ],
-              ),
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute<void>(
-                    builder: (context) => CounterScreen(index: index),
-                  ),
-                );
-              },
+class _CounterCard extends StatelessWidget {
+  const _CounterCard({required this.index});
+
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    final counters = context.watch<CountersCubit>().counters;
+    final counter = counters.at(index);
+
+    if (counter == null) {
+      return const SizedBox.shrink();
+    }
+
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      child: ListTile(
+        contentPadding: const EdgeInsetsDirectional.symmetric(
+          horizontal: 16,
+        ),
+        title: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton.outlined(
+              icon: const Icon(Icons.remove),
+              onPressed: () => counter.set(counter.get() - 1),
+            ),
+            const SizedBox(width: 8),
+            IconButton.outlined(
+              icon: const Icon(Icons.add),
+              onPressed: () => counter.set(counter.get() + 1),
+            ),
+            const SizedBox(width: 16),
+            Text(counter.get().toString()),
+          ],
+        ),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton.outlined(
+              onPressed: () => counters.removeAt(index),
+              isSelected: false,
+              icon: const Icon(Icons.delete),
+            ),
+            const SizedBox(width: 16),
+            const Icon(Icons.arrow_forward_ios),
+          ],
+        ),
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute<void>(
+              builder: (context) => CounterScreen(index: index),
             ),
           );
         },
