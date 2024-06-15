5d598b97e7f6ab6d312d11fdc78f0f79d8d95367
PiotrRogulski <piter.rogulski@gmail.com>
2024-06-15 22:51:55 +0200

    Update macro example

--- packages/bloc_lens_macros/example/bloc_lens_example.dart
+++ packages/bloc_lens_macros/example/bloc_lens_example.dart
@@ -1,23 +1,11 @@
 // ignore_for_file: avoid_print
 
-import 'dart:collection';
-
 import 'package:bloc/bloc.dart';
 import 'package:bloc_lens_macros/bloc_lens_macros.dart';
 
 void main() {
   print('\n=== MyRecordStateCubit ===');
   final myRecordStateCubit = MyRecordStateCubit();
-  print(myRecordStateCubit.state);
-  myRecordStateCubit.anInt.set(42);
-  myRecordStateCubit.anInt.increment();
-  myRecordStateCubit.aBool.toggle();
-  myRecordStateCubit.aList.set([1, 2, 3, 4, 5, 6]);
-  myRecordStateCubit.aList.at(2).set(69);
-  myRecordStateCubit.aMap.set({'foo': 1, 'bar': 2, 'baz': 3});
-  myRecordStateCubit.aMap['qux'].set(42);
-  myRecordStateCubit.anEnum.set(MyEnum.b);
-  myRecordStateCubit.anEnum.next();
   print(myRecordStateCubit.state);
   print(myRecordStateCubit.anEnum.values);
   print(
@@ -27,19 +15,21 @@ void main() {
       step: myRecordStateCubit.anInt.step,
     ),
   );
+  myRecordStateCubit.anInt.set(42);
+  myRecordStateCubit.anInt.increment();
+  myRecordStateCubit.aDouble.set(3.2);
+  myRecordStateCubit.aDouble.increment();
+  myRecordStateCubit.aBool.toggle();
+  myRecordStateCubit.aList.set([1, 2, 3, 4, 5, 6]);
+  myRecordStateCubit.aList[2]?.set(69);
+  myRecordStateCubit.aMap.set({'foo': 1, 'bar': 2, 'baz': 3});
+  myRecordStateCubit.aMap['qux'].set(42);
+  myRecordStateCubit.anEnum.set(MyEnum.b);
+  myRecordStateCubit.anEnum.next();
+  print(myRecordStateCubit.state);
 
   print('\n=== MyNamedStateCubit ===');
   final myNamedStateCubit = MyNamedStateCubit();
-  print(myNamedStateCubit.state);
-  myNamedStateCubit.anInt.set(42);
-  myNamedStateCubit.anInt.increment();
-  myNamedStateCubit.aBool.toggle();
-  myNamedStateCubit.aList.set([1, 2, 3, 4, 5, 6]);
-  myNamedStateCubit.aList.at(2).set(69);
-  myNamedStateCubit.aMap.set({'foo': 1, 'bar': 2, 'baz': 3});
-  myNamedStateCubit.aMap['qux'].set(42);
-  myNamedStateCubit.anEnum.set(MyEnum.b);
-  myNamedStateCubit.anEnum.next();
   print(myNamedStateCubit.state);
   print(myNamedStateCubit.anEnum.values);
   print(
@@ -49,41 +39,18 @@ void main() {
       step: myNamedStateCubit.anInt.step,
     ),
   );
-
-  print('\n=== MyAliasedStateCubit ===');
-  final myAliasedStateCubit = MyAliasedStateCubit();
-  print(myAliasedStateCubit.state);
-  myAliasedStateCubit.anInt.set(42);
-  myAliasedStateCubit.anInt.increment();
-  myAliasedStateCubit.aBool.toggle();
-  myAliasedStateCubit.aList.set([1, 2, 3, 4, 5, 6]);
-  myAliasedStateCubit.aList.at(2).set(69);
-  myAliasedStateCubit.aMap.set({'foo': 1, 'bar': 2, 'baz': 3});
-  myAliasedStateCubit.aMap['qux'].set(42);
-  myAliasedStateCubit.anEnum.set(MyEnum.b);
-  myAliasedStateCubit.anEnum.next();
-  print(myAliasedStateCubit.state);
-  print(myAliasedStateCubit.anEnum.values);
-  print(
-    (
-      min: myAliasedStateCubit.anInt.min,
-      max: myAliasedStateCubit.anInt.max,
-      step: myAliasedStateCubit.anInt.step,
-    ),
-  );
-}
-
-// throws: not a bloc
-// @MakeLenses()
-class NB extends ListBase<int> {
-  @override
-  dynamic noSuchMethod(Invocation invocation) {}
-}
-
-// throws: int is not an appropriate state type
-// @MakeLenses()
-class IntCubit extends Cubit<int> {
-  IntCubit() : super(0);
+  myNamedStateCubit.anInt.set(42);
+  myNamedStateCubit.anInt.increment();
+  myNamedStateCubit.aDouble.set(3.2);
+  myNamedStateCubit.aDouble.increment();
+  myNamedStateCubit.aBool.toggle();
+  myNamedStateCubit.aList.set([1, 2, 3, 4, 5, 6]);
+  myNamedStateCubit.aList[2]?.set(69);
+  myNamedStateCubit.aMap.set({'foo': 1, 'bar': 2, 'baz': 3});
+  myNamedStateCubit.aMap['qux'].set(42);
+  myNamedStateCubit.anEnum.set(MyEnum.b);
+  myNamedStateCubit.anEnum.next();
+  print(myNamedStateCubit.state);
 }
 
 @MakeLenses()
@@ -92,35 +59,7 @@ class MyNamedStateCubit extends Cubit<MyNamedState> {
       : super(
           MyNamedState(
             anInt: 0,
-            aBool: false,
-            aList: [],
-            aMap: {},
-            anEnum: MyEnum.a,
-          ),
-        );
-}
-
-@MakeLenses()
-class MyAliasedStateCubit extends Cubit<MyAlias> {
-  MyAliasedStateCubit()
-      : super(
-          MyAlias(
-            anInt: 0,
-            aBool: false,
-            aList: [],
-            aMap: {},
-            anEnum: MyEnum.a,
-          ),
-        );
-}
-
-// throws: no unnamed constructor
-// @MakeLenses()
-class MyAliasedAltStateCubit extends Cubit<MyAliasAlt> {
-  MyAliasedAltStateCubit()
-      : super(
-          MyAliasAlt.foobar(
-            anInt: 0,
+            aDouble: 0,
             aBool: false,
             aList: [],
             aMap: {},
@@ -135,6 +74,7 @@ class MyRecordStateCubit extends Cubit<MyNamedRecordState> {
       : super(
           (
             anInt: 0,
+            aDouble: 0.0,
             aBool: false,
             aList: [],
             aMap: {},
@@ -143,18 +83,12 @@ class MyRecordStateCubit extends Cubit<MyNamedRecordState> {
         );
 }
 
-// throws: has positional fields
-// @MakeLenses()
-class MyInlineRecordStateCubit
-    extends Cubit<(int anInt, String, {bool aBool})> {
-  MyInlineRecordStateCubit() : super((0, 'foo', aBool: false));
-}
-
 enum MyEnum { a, b, c }
 
 class MyNamedState {
   MyNamedState({
     required this.anInt,
+    required this.aDouble,
     required this.aBool,
     required this.aList,
     required this.aMap,
@@ -163,6 +97,8 @@ class MyNamedState {
 
   @NumberOptions(min: -5, max: 15, step: 2)
   final int anInt;
+  @NumberOptions(min: -12.5, max: 12.5, step: 0.5)
+  final double aDouble;
   final bool aBool;
   final List<int> aList;
   final Map<String, int> aMap;
@@ -170,34 +106,15 @@ class MyNamedState {
 
   @override
   String toString() {
-    return 'MyNamedState(anInt: $anInt, aBool: $aBool, aList: $aList, aMap: $aMap, anEnum: $anEnum)';
+    return 'MyNamedState(anInt: $anInt, aDouble: $aDouble, aBool: $aBool, aList: $aList, aMap: $aMap, anEnum: $anEnum)';
   }
 }
-
-class MyNamedStateAlt {
-  MyNamedStateAlt.foobar({
-    required this.anInt,
-    required this.aBool,
-    required this.aList,
-    required this.aMap,
-    required this.anEnum,
-  });
-
-  final int anInt;
-  final bool aBool;
-  final List<int> aList;
-  final Map<String, int> aMap;
-  final MyEnum anEnum;
-}
-
-typedef MyAlias = MyNamedState;
-
-typedef MyAliasAlt = MyNamedStateAlt;
 
 typedef MyNamedRecordState = MyNamedRecordState2;
 
 typedef MyNamedRecordState2 = ({
   int anInt,
+  double aDouble,
   bool aBool,
   List<int> aList,
   Map<String, int> aMap,
